0455b517aa99d9f9b355a49f08a32aefcf928b51
João Nascimento <joaoif2020@hotmail.com>
2023-10-03 21:10:37 -0400

    insert data for test

---
+++ data.js
@@ -0,0 +1,28 @@
+//Todo List
+const todos = [
+    {
+        id: 1,
+        title: "Coding in Javascript",
+        description: "Working with functions in Javascript",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Lunch",
+        description: "Preparing miojo and feijão",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Gym",
+        description: "Leg day today",
+        completed: false,
+    },
+    {
+        id: 4,
+        title: "Watching Netflix",
+        description: "Enjoying the new dorama",
+        completed: false,
+    },
+];
+module.exports = todos;
